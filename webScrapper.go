286c9c134998b214fcca0053c3dc00dafc01b56a
pedro <pedrog.2016@hotmail.com>
2025-05-02 18:43:02 -0300

    sw :wq :wq :wq
    :x
    _x
    :x
    :x
    :wq
    asdas
    :wq

--- webScrapper.go
+++ webScrapper.go
@@ -3,10 +3,16 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"gopkg.in/goquery.v1"
 )
 
 func main() {
-	fmt.Println("cibola")
-	resp, err := http.Get("https://eva.fing.edu.uy/mod/forum/view.php?id=217947")
+	resp, err = http.Get("https://eva.fing.edu.uy/mod/forum/view.php?id=217947")
+	doc, err= goquery.NewDocumentFromReader(resp.Body)
+	doc.Find("td.topic.starter.a").Each(func(i int, s *goquery.Selection)){
+		titulo := s.Text()
+		fmt.PrintLn("titulo: ",titulo)
+
+	}
 
 }
