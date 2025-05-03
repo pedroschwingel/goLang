8df822d106c314a4b410a2f243a447dc6f7f4f1e
peschwin <pedrog.2016@hotmail.com>
2025-05-02 23:00:23 -0300

    pedro lindo

--- go.mod
+++ go.mod
@@ -1,3 +1,11 @@
-module net/http
+module webScrapper
 
 go 1.24.2
+
+require gopkg.in/goquery.v1 v1.0.1
+
+require (
+	github.com/PuerkitoBio/goquery v1.10.3 // indirect
+	github.com/andybalholm/cascadia v1.3.3 // indirect
+	golang.org/x/net v0.39.0 // indirect
+)
--- webScrapper.go
+++ webScrapper.go
@@ -1,18 +1,49 @@
 package main
 
 import (
+	//"container/list"
 	"fmt"
 	"net/http"
+	"strings"
 	"gopkg.in/goquery.v1"
 )
 
-func main() {
-	resp, err = http.Get("https://eva.fing.edu.uy/mod/forum/view.php?id=217947")
-	doc, err= goquery.NewDocumentFromReader(resp.Body)
-	doc.Find("td.topic.starter.a").Each(func(i int, s *goquery.Selection)){
-		titulo := s.Text()
-		fmt.PrintLn("titulo: ",titulo)
 
+func obtenerUrls()[]*http.Response{
+	var paginas []*http.Response
+	var link string
+	for{ 
+		fmt.Println("Ingrese el Url del foro al cual desea suscribirse o 0 para salir:")
+		fmt.Scanln(&link)
+		if link == "0" {
+			break
+		}
+		resp,err := http.Get(link)
+		if err != nil {
+			fmt.Println("error, intente nuevamente")
+			}else{
+				paginas = append(paginas, resp)
+			}
+		}
+		return paginas
+}
+
+func main() {
+	pages := obtenerUrls()
+	fmt.Println(len(pages))
+	for i := 0; i < len(pages); i++ {
+		doc, err := goquery.NewDocumentFromReader(pages[i].Body)
+		if err!=nil {
+			fmt.Println("bad things")
+		}
+		nombreCurso :=doc.Find(".breadcrumb-item").Eq(1)
+		fmt.Println("nombre del curso es:", nombreCurso.Text())
+		fmt.Println("las ultimas 5 actualizaciones fueron:")
+		dis := doc.Find("tr.discussion th a").Slice(0,5)
+		dis.Each(func(index int, element *goquery.Selection)  {
+			fmt.Println(strings.TrimSpace(element.Text()))
+		})
 	}
 
 }
+ 
